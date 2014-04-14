cb040d310ee6f2bf129ddcdcd22585ee9c7e163c
eskat0n <muyou.prj@gmail.com>
2014-04-14 15:57:21 +0600

    added logging handling

--- tools/downloader.rb
+++ tools/downloader.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'iron_mq'
+
+require 'log4r'
+require 'log4r/yamlconfigurator'
 require 'awesome_print'
 
 require_relative 'classes/anime'
@@ -9,7 +12,15 @@ require_relative 'classes/episode'
 require_relative 'classes/myvi_episode'
 require_relative 'classes/sibnet_episode'
 
+include Log4r
+
+logger_config = File.join(File.dirname(__FILE__), 'log4r.yml')
+YamlConfigurator.load_yaml_file logger_config
+
+logger = Logger.new('default')
 ironmq = IronMQ::Client.new
+
+logger.info 'Bogie download daemon started'
 
 queue = ironmq.queue('download_tasks')
 queue.poll do |message|
@@ -23,3 +34,4 @@ queue.poll do |message|
   ap episodes
 end
 
+logger.info 'Bogie download daemon stopped'
